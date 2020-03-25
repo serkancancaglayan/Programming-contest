b92a802d53f663e4d33db78ef47f9c04f8874b64
Serkan Can Caglayan <49690023+serkancancaglayan@users.noreply.github.com>
2020-03-25 12:05:37 +0300

    Add files via upload

---
+++ monk.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h> // std::find()
+int main(){
+	int t;
+	std::cin >> t;
+	for(auto i = 0; i < t; i++){
+		int m, n;
+		std::cin >> n >> m;
+		std::vector<int> vn;
+		for(auto j = 0; j < n; j++){
+			int temp;
+			std::cin >> temp;
+			vn.push_back(temp);
+		}
+		for(auto j = 0; j < m; j++){
+			int temp;
+			std::cin >> temp;
+			if(std::find(vn.begin(), vn.end(), temp) != vn.end()){
+				std::cout << "YES" << std::endl;
+			}else{
+				std::cout<< "NO" << std::endl;
+				vn.push_back(temp);
+			}
+		}
+	}
+
+	return 0;
+}
