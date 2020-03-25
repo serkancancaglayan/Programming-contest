11959f485ab8d4f9aac6b361b5311e99c4e36ec4
Serkan Can Caglayan <49690023+serkancancaglayan@users.noreply.github.com>
2020-03-25 22:23:28 +0300

    Add files via upload

--- left_shift.py
+++ left_shift.py
@@ -1,9 +1,8 @@
-arr_size = int(input())
-shift_count = int(input())
+arr_size, shift_count = [int(item) for item in input().split(" ")]
 array = [int(item) for item in input().split(" ")]
 
 for i in range(shift_count):
     array.append(array.pop(0))
 
-print(array)
-
+for i in range(arr_size):
+    print(array[i], sep = " ", end =" ")
